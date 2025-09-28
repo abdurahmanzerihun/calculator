706810f023103845db07d79429801e354bf732b5
abdurahman <abdurrahmanzerihun@gmail.com>
2025-09-28 10:56:29 +0300

    Modify and simplify  the logic
    I ve Modified the logic so that the calculator can perform
    long expressions and simplified it for clearer look
    (like , using switch and cases instead of if else
     if and removing the operation functions(add,sub, etc.)
      and defining them inside operate function.)

--- script.js
+++ script.js
@@ -1,79 +1,59 @@
 let display=document.getElementById('display');
 let digitBtn=document.querySelectorAll('button');
-let firstNumber;
-let operator;
-let secondNumber;
+let firstNumber=null;
+let operator=null;
+let secondNumber=null;
+let operatorsArr=['+','*','/','-'];
+//let expression=display.value;
+
 
 digitBtn.forEach(button=>{
         button.addEventListener('click',()=>{
-                let digitValue=button.textContent;
-                
-                if(digitValue==='C'){
-                        display.value="";
+            let digitValue=button.textContent;   
+              
+        if(!isNaN(digitValue)) {
+                display.value +=digitValue;
+               if(operator===null){
+        firstNumber=Number(display.value);
+        }
+                 else {
+        secondNumber=Number(display.value.split(operator).pop());
+        }
+        }
+        else if(operatorsArr.includes(digitValue)){
+                if(firstNumber!==null && operator!==null && secondNumber!==null)//something ,for eg.,12[operator]3 and you pressed one of the operator again. 
+                        {
+                        firstNumber=operate(firstNumber,operator,secondNumber);
+                        display.value=firstNumber;//show intermidate result 
+                        secondNumber=null;
                 }
-                else if(digitValue==='='){
-                        const expression=display.value;
-                        if(expression.includes('+'))
-                        operator='+';
-                        else if(expression.includes('-'))
-                        operator='-';
-                        else if(expression.includes('*'))
-                        operator='*';
-                        else if(expression.includes('/'))
-                        operator='/';
-                let part=expression.split(operator);
-                firstNumber=Number(part[0]);
-                secondNumber=Number(part[1]);
-                display.value=operate(firstNumber,operator,secondNumber);
+                operator=digitValue;             /*assigning the button textContent and display it  with what is there in the input box  
+                                                if the above condition is not satisfied */
+                display.value +=operator;
+        }                                      
+        else if(digitValue==='='){
+                if(firstNumber!==null && operator!==null && secondNumber!==null){
+                  display.value=operate(firstNumber,operator,secondNumber);
+                firstNumber=Number(display.value);
+                secondNumber=null;
+                operator=null;      
                 }
-                else {
-                        display.value+=digitValue;
-                }
+        }
+        else if(digitValue==='C'){
+                display.value="";
+                firstNumber=secondNumber=operator=null;
+        }
+
                 
-                
-        })
-})
-function add(firstNumber,secondNumber){
-return firstNumber+secondNumber;
-
-}
-function sub(firstNumber,secondNumber){
-return firstNumber-secondNumber;
-
-}
-function mult(firstNumber,secondNumber){
-return firstNumber*secondNumber;
-
-}
-function div(firstNumber,secondNumber){
-return firstNumber/secondNumber;
-
-}
+        });
+});
 
 function operate(firstNumber,operator,secondNumber ){
-        if(operator==='+'){
-               return add(firstNumber,secondNumber);
+        switch(operator){
+                case'+': return  firstNumber+secondNumber;
+                case '-': return firstNumber-secondNumber;
+                case '*': return firstNumber*secondNumber;
+                case '/': return firstNumber/secondNumber;
         }
-        else if(operator==='-'){
-                return sub(firstNumber,secondNumber);
-        }
-        else if(operator==='*'){
-               return mult(firstNumber,secondNumber );
-        }
-        else if(operator==='/'){
-                return div(firstNumber,secondNumber);
-        }
-
 }
-
-
-/*let a=parseInt(prompt('Enter the first number',''))
-let b=parseInt(prompt('Enter the second number',''))
-let x=add(a,b);
-let y=sub(a,b);
-let z=mult(a,b);
-let w=div(a,b);
-alert(`${a}+${b}=${x}`);
-alert(`${a}-${b}=${y}`);
-alert(`${a}*${b}=${z}`);
-alert(`${a}/${b}=${w}`);*/
+       
